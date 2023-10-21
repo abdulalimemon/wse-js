3924fa07bae3cae467746e3b2b8254a122239c95
emon02 <abdulalimemon11@gmail.com>
2023-10-21 18:56:41 +0600

    content uploaded

---
+++ starter/3-problems.js
@@ -0,0 +1,8 @@
+// write a function that returns true if a number is even and false if a number is odd.
+
+// Create a function isLeapYear that takes a year as a parameter and returns true if it's a leap year or false if it's not.
+
+// Find the sum of the numbers from 1 to 10 (using loop and function).
+
+// Create a function calculateFactorial that takes a positive integer as a parameter and calculates and returns its factorial.
+
