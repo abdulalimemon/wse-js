31179e7a40b7927a79eb0f96364f4bb7d09dbeea
emon02 <abdulalimemon11@gmail.com>
2023-10-23 22:00:55 +0600

    assignment

---
+++ assignment/assignment.js
@@ -0,0 +1,46 @@
+/*
+=> Solve these questions.
+
+Problem 1: Write a function to calculate the area of a triangle.
+
+Problem 2: Write a function to convert degrees to radians.
+ 
+Problem 3: Create a function calculateFactorial that takes a number and returns its factorial.
+
+Problem 4: Create a function isPrime that takes a number as a parameter and returns true if it's a prime number, and false otherwise.
+
+Problem 5: Create a function mergeArrays that takes two arrays as parameters and returns a new array that merges both arrays.
+
+Problem 6: Create a function isLeapYear that takes a year as a parameter and returns "This is a leap year" if it's a leap year, and "Not Leap year" otherwise.
+
+Problem 7: Create a function removeDuplicates that takes an array and returns a new array with duplicates removed.
+
+Problem 8: Create a function convertToCelsius that takes a temperature in Fahrenheit and returns the equivalent temperature in Celsius.
+
+Problem 9: Write a function to find the maximum of five numbers.
+
+Problem 10: Create a function called evenOdd() that takes a string as a parameter. Now you have to give the output based on the total number of characters in your String. The output will be 'even' or 'odd'. [ Input: ‘JavaScript’ Output: even, Input: ‘Hello’ Output: odd]
+
+
+
+
+=> Answer any 4 questions.
+
+1. Explain the difference between 'if...else' and 'switch' statements for conditional logic.
+
+2. What is JavaScript, and what is its primary purpose in web development?
+
+3. Explain the difference between var, let, and const when declaring variables in JavaScript.
+
+4. Explain the concept of "scope" in JavaScript and the difference between global and local scope.
+
+5. What is the difference between "null" and "undefined" in JavaScript?
+
+
+
+=> Assignment Deadline + Marks:
+1. Each question value is 5 marks.
+2. The deadline to submit your assignment is October 25, 2023, at 8:00 PM.
+
+
+*/
